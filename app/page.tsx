38aae6c7e75d2aadbf862ead1980526a67c12db0
aryankumar07 <ak2839426@gmail.com>
2024-10-19 05:13:31 +0530

    basics

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-'use client'
+"use client";
 import { FieldValues, SubmitHandler } from "react-hook-form";
 import { useForm } from "react-hook-form";
-
+import axios from 'axios'
 
 const Home = () => {
-
   const {
     register,
     handleSubmit,
@@ -13,11 +12,24 @@ const Home = () => {
   } = useForm<FieldValues>({
     defaultValues: {
       name: "",
+      type: "",
     },
   });
 
+
+  const fetchdata = (data : FieldValues)=>{
+    if(data.type==="Leetcode"){
+      axios.get('/api/scrap/leetcode',data)
+      .then(()=>{})
+      .catch((error)=>{})
+      .finally(()=>{})
+    }else{
+      console.log('working on it')
+    }
+  }
+
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    console.log(data);
+    fetchdata(data)
   };
 
   return (
@@ -26,16 +38,39 @@ const Home = () => {
         onSubmit={handleSubmit(onSubmit)}
         className="flex flex-col items-start gap-1"
       >
-        <input type="text" {...register("name", { required: true })} className="text-black pl-2" placeholder="name"/>
-        {errors.username && (
-          <p className="text-red-500">Username is required</p>
-        )}
+        <div className="flex gap-1 text-black">
+          <input
+            type="text"
+            {...register("name", { required: true })}
+            className="text-black pl-2"
+            placeholder="name"
+          />
+
+          <select
+            {...register("type", { required: true })}
+            className="text-black"
+          >
+            <option value="">Select Type</option>
+            <option value="Leetcode">Leetcode</option>
+            <option value="GeeksForGeeks">GeeksForGeeks</option>
+            <option value="Striver">Striver</option>
+            <option value="CodeForces">CodeForces</option>
+          </select>
+        </div>
+
+        {/* Error message for name */}
+        {errors.name && <p className="text-red-500">Username is required</p>}
+
+        {/* Error message for type */}
+        {errors.type && <p className="text-red-500">Type is required</p>}
+
+        {/* Submit button */}
         <button type="submit" className="bg-white text-black w-2/3">
           Search
         </button>
       </form>
     </div>
   );
-}
+};
 
 export default Home;
