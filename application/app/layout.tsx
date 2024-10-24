bc18bc4881cc55f878f8aa8ab14616f5ea990cd1
aryankumar07 <ak2839426@gmail.com>
2024-10-24 15:44:06 +0530

    refactor: Setting Basic Skelton
    removed the old layout and made refactored the main page layout with reusable components with responsive
    web-design

--- application/app/layout.tsx
+++ application/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { inter } from "./fonts";
+import ClientOnly from "@/components/clientonly";
+import MainPage from "@/components/mainpage";
 
 export const metadata: Metadata = {
   title: "OneProfile",
@@ -16,7 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={cn(inter.className, "antialiased")}
+        className={cn(
+          inter.className,
+          "antialiased overflow-y-hidden "
+        )}
       >
         {children}
       </body>
