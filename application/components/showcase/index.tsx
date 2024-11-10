0e03b71daf8d53d970af5161bf225fe15ad69eea
Dharmesh53 <dharmeshwr@gmail.com>
2024-11-10 16:29:34 +0530

    feat(DB): add prisma models

--- application/components/showcase/index.tsx
+++ application/components/showcase/index.tsx
@@ -1,17 +1,54 @@
 'use client'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import CardWrapper from '@/components/showcase/card-wrapper'
 import SkeletonBody from './skeleton-body'
 import { getSVG } from '@/lib/getSVG'
 import { OPTIONS } from '@/lib/constants'
 import axios from 'axios'
 import Profiles from './profiles'
+import prisma from "db"
 
 type DataType = {
   [key: string]: Record<string, unknown>
 }
 
 const Showcase = () => {
+  const { loading, data, onClick } = useShowCaseStates()
+
+  useEffect(() => {
+  }, [])
+
+  const renderCard = (header: string, type: string) => (
+    <CardWrapper
+      header={header}
+      icon={getSVG(type)}
+      type={type}
+      onClick={onClick}
+      loading={loading}
+    >
+      {Object.keys(data[type]).length > 0
+        ? <Profiles type={type} data={data[type]} />
+        : <SkeletonBody profile={type} />
+      }
+    </CardWrapper>
+  )
+
+  return (
+    <div className="flex flex-col w-full gap-7 justify-center p-5 max-w-[1500px]">
+      <div className="flex w-full gap-7 max-[1200px]:flex-wrap">
+        {renderCard("Ninjafire", OPTIONS.LEETCODE)}
+        {renderCard("riyanshig816", OPTIONS.GFG)}
+      </div>
+      <div className="flex w-full gap-7 max-[900px]:flex-wrap">
+        {renderCard("tourist", OPTIONS.CODEFORCES)}
+        {renderCard("aryan070", OPTIONS.CODECHEF)}
+        {renderCard("someone69", OPTIONS.STRIVER)}
+      </div>
+    </div>
+  )
+}
+
+const useShowCaseStates = () => {
   const [loading, setLoading] = useState<string | null>(null)
   const [data, setData] = useState<DataType>({
     "Leetcode": {},
@@ -61,34 +98,7 @@ const Showcase = () => {
     setLoading(null)
   }
 
-  const renderCard = (header: string, type: string) => (
-    <CardWrapper
-      header={header}
-      icon={getSVG(type)}
-      type={type}
-      onClick={onClick}
-      loading={loading}
-    >
-      {Object.keys(data[type]).length > 0
-        ? <Profiles type={type} data={data[type]} />
-        : <SkeletonBody profile={type} />
-      }
-    </CardWrapper>
-  )
-
-  return (
-    <div className="flex flex-col w-full gap-7 justify-center p-5 max-w-[1500px]">
-      <div className="flex w-full gap-7 max-[1200px]:flex-wrap">
-        {renderCard("Ninjafire", OPTIONS.LEETCODE)}
-        {renderCard("riyanshig816", OPTIONS.GFG)}
-      </div>
-      <div className="flex w-full gap-7 max-[900px]:flex-wrap">
-        {renderCard("tourist", OPTIONS.CODEFORCES)}
-        {renderCard("aryan070", OPTIONS.CODECHEF)}
-        {renderCard("someone69", OPTIONS.STRIVER)}
-      </div>
-    </div>
-  )
+  return { loading, data, onClick }
 }
 
 export default Showcase
